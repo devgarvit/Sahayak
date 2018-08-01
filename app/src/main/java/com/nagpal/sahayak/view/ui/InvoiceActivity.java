23702450b367bf79f216057e4de0e3cb1c5d7dcc
snehagpt923 <sneha.gupta@railyatri.in>
2018-08-01 23:09:30 +0530

    added data binding to add invoice screen

--- app/src/main/java/com/nagpal/sahayak/view/ui/InvoiceActivity.java
+++ app/src/main/java/com/nagpal/sahayak/view/ui/InvoiceActivity.java
@@ -1,15 +1,22 @@
 package com.nagpal.sahayak.view.ui;
 
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 import com.nagpal.sahayak.R;
+import com.nagpal.sahayak.databinding.ActivityInvoiceBinding;
 
 public class InvoiceActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_invoice);
+
+        ActivityInvoiceBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_invoice);
+
+        binding.textViewName.setText("DD Enterprises");
+        binding.textViewAddress.setText("983, Karol Bagh, New  Delhi Road, near Metro Station- 110023");
+
     }
 }
